d292ccd30f88233a6831156cf5e6d3c94d06b59e
Franco Galfre <francogalfre2007@gmail.com>
2022-10-08 15:12:16 -0300

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,54 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
+import { AppProps } from "next/app";
+import { getCookie, setCookies } from "cookies-next";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+  Slider,
+} from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
+import { rtlCache } from "rtl-cache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookies("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <>
+      <div dir="rtl">
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            theme={{ colorScheme, dir: "rtl" }}
+            withGlobalStyles
+            withNormalizeCSS
+            emotionCache={rtlCache}
+          >
+            <NotificationsProvider>
+              <Component {...pageProps} />
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
